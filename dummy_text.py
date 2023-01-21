2c97c49fde65a277debf5d89fc5a382c81b7021c
Shadylhawary <shadylhawary@gmail.com>
2023-01-21 12:18:49 +0200

    Init new Py file to store strings over the app

---
+++ dummy_text.py
@@ -0,0 +1,16 @@
+
+dep_kilo = 'Depreciation X kilometrage is an allowance for the decline in value of a car due the distance traveled. There are several approaches to calculate the depreciation value for a certain car make, data analysis enables us to easily calculate and visualize the depreciation value for different car makes & models instantly as well we can compare different models to get the desired result.'
+note1 = "\U0001F539	 Ideal car which has a zero slope trendline in (Kilometer, Price) graph. That's means the value of the car does not related to the distance traveled."
+
+
+dep_year = "Every car loses value over time except for classic and collectible cars, which may be worth a lot more than when they were new. Depreciation is why used cars are usually cheaper than new cars. The used car price reflects the loss of value since the car rolled off the dealer's lot for the first time. A used car will continue to depreciate, but usually not as fast as a new car. Car lease rates are based in part on the depreciation of the car during the term of the lease."
+note2 = "\U0001F539	 How Much Value Will Your New Car Lose?"
+
+Q1 = "\U00002049 How Many Unused Kilometers Does Your Car Have Left?"
+Ans1 = "When you go to sell your car, buyers will essentially be paying for the unused kilometers it has left. If you have a reliable vehicle that can be expected to last for 200,000 kms and you go to sell at 100,000 kms, it should still have half its useful life left."
+
+Q2 = "\U00002049 Kilometrage and Model Year Are the Only Factors in Depreciation and Valuation?"
+Ans2 = "Certain makes, models and vehicle characteristics are more highly valued by car buyers. Vehicles that are more highly valued depreciate at slower rates, regardless of kilometrage. Some of those factors include (Vehicle's Reputation, Fuel Economy, Type, Condition)."
+
+Q3 = "\U00002049 Should You Let Depreciation Influence Your Car Buying Choice?"
+Ans3 = "If you’re buying a new car, you should consider factoring in depreciation when choosing a car to buy. However, if you’re buying a used vehicle the calculation is different. Most pre-owned car buyers are purchasing vehicles that have already gone through the worst of their depreciation."
